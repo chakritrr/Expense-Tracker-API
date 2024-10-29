deb561d20a5fb37f4e043e3d4afd2d9fa5d32079
chakritrr <127234504+chakritrr@users.noreply.github.com>
2024-10-29 16:33:17 +0700

    Update: expense controller

--- src/controller/expense/expense.controller.ts
+++ src/controller/expense/expense.controller.ts
@@ -1,14 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+
 import { PostExpenseRequestDto } from 'src/core';
+import { ExpenseCreateUseCase } from 'src/use-case/expense-create/expense-create-use-case';
 
 @ApiTags('Expense-Controller')
 @Controller()
 export class ExpenseController {
-  constructor() {}
+  constructor(private readonly expenseCreateUseCase: ExpenseCreateUseCase) {}
 
   @Post('/v1/expense')
   postExpense(@Body() postExpenseRequestDto: PostExpenseRequestDto) {
-    return postExpenseRequestDto;
+    return this.expenseCreateUseCase.createExpense(postExpenseRequestDto);
   }
 }
