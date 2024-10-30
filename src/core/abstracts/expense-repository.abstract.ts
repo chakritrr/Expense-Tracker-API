7f16f90e02b8ff00b5ddaffd435dde6ee58422d4
chakritrr <127234504+chakritrr@users.noreply.github.com>
2024-10-31 00:11:07 +0700

    Update: expense abstract and repository

--- src/core/abstracts/expense-repository.abstract.ts
+++ src/core/abstracts/expense-repository.abstract.ts
@@ -2,4 +2,6 @@ import { ExpenseEntity } from '../entities';
 
 export abstract class IExpenseRepository {
   abstract findAll(): Promise<ExpenseEntity[]>;
+
+  abstract findOneByIdRelation(id: string): Promise<ExpenseEntity>;
 }
